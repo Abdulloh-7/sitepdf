bb41411525e80edbdb30b6c64b46e29fbfb27853
Ivan Petrov <ivanpetrov@gmail.com>
2026-03-19 17:23:54 +0500

    обновил HTML + backend

--- backend.py
+++ backend.py
@@ -31,8 +31,7 @@ def get_file():
     except FileNotFoundError:
         return jsonify({"error": "Файл на сервере не найден"}), 404
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render задаёт порт через переменную PORT
     app.run(host="0.0.0.0", port=port, debug=True)
